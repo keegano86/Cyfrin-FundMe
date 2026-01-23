4d44d6cf30194dc17c2636989096885da9e201b1
keegano86 <keeganoloughlin@gmail.com>
2026-01-23 14:35:26 -0600

    Adding addresses for  chainlink

--- FundMe.sol
+++ FundMe.sol
@@ -7,20 +7,26 @@
 pragma solidity ^0.8.31;
 
 contract FundMe {
+
+    uint256 public minimumusd = 5;
+
     function fund() public payable {
         // Allow users to send $
         //Have a minimum $ Sent
         // 1. How do we send Eth to this contract
-        require(msg.value > 1e18, "didn't send enough Ether");
+        require(msg.value >= minimumusd, "didn't send enough!");
         // a ton of computation
 
         // What is a revert?
         // undo any actions that have been done, and send the remaining gas back
     }
 
-
-
-
     // function withdraw() public {}
+
+    function getPrice() public {
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI
+    }
+    function getConversionRate() public {}
 
 }
